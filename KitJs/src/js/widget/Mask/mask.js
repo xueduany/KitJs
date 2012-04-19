ddab25b16f63fbd8ea5c0239248fb2a88bc5b821
xueduanyang <xueduanyang1985@163.com>
2012-04-19 18:40:08 +0800

    加上遮罩

---
+++ KitJs/src/js/widget/Mask/mask.js
@@ -0,0 +1,69 @@
+/**
+ * 半透明的遮罩
+ */
+$kit.ui.Mask = function(config) {
+	var me = this;
+	me.config = $kit.join(me.constructor.defaultConfig, config);
+	var what = $kit.newHTML($kit.tpl(me.config.what, me.config)).childNodes[0];
+	document.body.appendChild(what);
+	me.what = what;
+	me.animTimeout = 'kitMask_' + $kit.onlyId();
+	if(me.config.anim) {
+		$kit.anim.motion({
+			el : me.what,
+			from : {
+				opacity : 0
+			},
+			to : {
+				opacity : 0.5
+			},
+			timeout : me.animTimeout
+		});
+		me.what = what;
+	}
+}
+$kit.ui.Mask.defaultConfig = {
+	where : null,
+	pos : 'last',
+	what : [//
+	'<div style="', //
+	'display:block;', //
+	'position:fixed;', //
+	'_position:absolute;', //
+	'top:0;', //
+	'left:0;', //
+	'width:100%;', //
+	'height:100%;', //
+	'_width:expression(eval(document.compatMode &&', //
+	'document.compatMode == \'CSS1Compat\' ?', //
+	'document.documentElement.scrollLeft + document.documentElement.clientWidth', //
+	': document.body.scrollTop + document.body.clientWidth));', //
+	'_height:expression(eval(document.compatMode &&', //
+	'document.compatMode == \'CSS1Compat\' ?', //
+	'document.documentElement.scrollTop + document.documentElement.clientHeight', //
+	': document.body.scrollTop + document.body.clientHeight));', //
+	'zoom:1;', //
+	'background:${color};', //
+	'">', //
+	'<!--[if lte IE 6.5]><iframe src="javascript:false" style="', //
+	'position:absolute;', //
+	'top:0;', //
+	'left:0;', //
+	'width:100%;', //
+	'height:100%;', //
+	'z-index:-1;', //
+	'border:none;', //
+	'filter: mask();', //
+	'"></iframe><![endif]-->', //
+	'</div>'//
+	].join(''),
+	color : '#000', //遮罩颜色
+	opacity : 0.75, //透明度
+	anim : true//是否使用动画
+}
+$kit.merge($kit.ui.Mask.prototype, {
+	destory : function() {
+		clearInterval(window[this.animTimeout]);
+		$kit.rmEl(this.what);
+	}
+});
