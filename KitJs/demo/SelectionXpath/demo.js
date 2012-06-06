6a73bebed658b00508edb45b7cddb88273d16f1d
xueduanyang <xueduanyang1985@163.com>
2012-06-06 14:57:28 +0800

    update range demo

--- KitJs/demo/SelectionXpath/demo.js
+++ KitJs/demo/SelectionXpath/demo.js
@@ -14,15 +14,14 @@ $kit.$(function() {
 				// console.log($kit.selection.getEndContainer(r));
 				// console.log($kit.selection.getEndOffset(r));
 				// return;
-				var mark = '';
+				var mark = $kit.selection.getXpath($kit.selection.getRange()).full;
 				var config = $kit.selection.getXpath($kit.selection.getRange());
-				if(console) {
-					mark = $kit.selection.getXpath($kit.selection.getRange()).full;
+				if(window.console) {
 					console.log(mark);
+					console.log('换个浏览器试试在console输入:$kit.selection.highlight("' + $kit.selection.getXpath($kit.selection.getRange()).full + '")');
+					console.log('如果出现部分误差，和浏览器的渲染模式有关，因为有的浏览器渲染出来有空白，有的没有，纯小说文本，完全无问题');
 				}
-				console && console.log($kit.selection.getXpath($kit.selection.getRange()).full);
-				console && console.log('换个浏览器试试在console输入:$kit.selection.highlight("' + $kit.selection.getXpath($kit.selection.getRange()).full + '")');
-				console && console.log('如果出现部分误差，和浏览器的渲染模式有关，因为有的浏览器渲染出来有空白，有的没有，纯小说文本，完全无问题');
+				//console && console.log($kit.selection.getXpath($kit.selection.getRange()).full);
 				alert($kit.selection.highlight(config));
 				window.open('?config=' + mark);
 			}
